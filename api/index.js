d923797cd5c68691601e5f972a89185a8edb389a
Rad <Rad@radski.eu>
2022-08-28 23:22:42 +0200

    Error handling

--- api/index.js
+++ api/index.js
@@ -1,22 +1,32 @@
 /** @type {import("express").RequestHandler} */
-import { express } from 'express';
-import dotenv from 'dotenv';
-import auth from './src/controller/auth.js'
-
-dotenv.config();
-
+import express from 'express';
+import 'dotenv/config';
+import auth from './src/controllers/auth.js'
+import tank from './src/controllers/tank.js'
+import 'express-async-errors';
 const app = express();
+app.use(express.json())
 
-const errorHandling = (err, req, res) => {
+process.env.NODE_ENV = 'production'
+
+const errorHandling = (err, req, res, next) => {
   res.status(500).json({
     message: err.message,
     error: true
   })
+  next()
 }
 
-app.use('/auth', auth)
+app.get('/', async (req, res) => {
+  await new Promise((reject, resolve )=> {
+    throw new Error('owo')
+  })
+})
 
-app.use('/', errorHandling)
+app.use('/auth', auth)
+app.use('/tank', tank)
+app.use(errorHandling)
+
 
 app.listen(8000, () => {
   console.log('App listening on 8000');
